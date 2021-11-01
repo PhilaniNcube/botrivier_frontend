2a2e9c5022011741c2fbd660a63527a2ccea1400
Philani Ncube <ncbphi001@gmail.com>
2021-11-01 17:01:24 +0200

    api url

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+  process.env.NEXT_PUBLIC_API_URL || 'https://botriverfrontend.vercel.app';
 
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://botrivier.herokuapp.com/';
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://botriverfrontend.vercel.app';
 
 export const PER_PAGE = 12;
