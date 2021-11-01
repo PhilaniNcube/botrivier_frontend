3ec68ccf882fe8f6b373d443775cbf843284d24e
Philani Ncube <ncbphi001@gmail.com>
2021-11-01 16:48:09 +0200

    next_url

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
 
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://botrivier.herokuapp.com/';
 
 export const PER_PAGE = 12;
