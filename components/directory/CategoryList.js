459b6e7071f5d81960ba4c64f4b9d565e1aa29f5
Philani Ncube <ncbphi001@gmail.com>
2021-11-28 21:50:55 +0200

    category layout

--- components/directory/CategoryList.js
+++ components/directory/CategoryList.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const CategoryList = ({ categories }) => {
   return (
-    <section className="md:px-8 dark:bg-coolGray-800 dark:text-coolGray-100">
+    <section className="md:px-8 dark:bg-coolGray-800 dark:text-coolGray-100 mx-auto max-w-7xl">
       <div className="container grid grid-cols-1 gap-6 m-4 mx-auto md:m-0 md:grid-cols-2 xl:grid-cols-6 py-2">
         {categories.map((category) => {
           return (
