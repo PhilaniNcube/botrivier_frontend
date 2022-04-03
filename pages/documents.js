932c05c860d167a949da57a5fa7255694cfeeb23
Philani Ncube <ncbphi001@gmail.com>
2022-04-03 16:27:56 +0200

    documents

--- pages/documents.js
+++ pages/documents.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { Fragment } from 'react';
 import { DocHero } from '../components/documents/DocHero';
 
@@ -5,6 +6,18 @@ const documents = () => {
   return (
     <Fragment>
       <DocHero />
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 py-12">
+        <Link href="/images/directors.pdf" passHref>
+          <button className="text-base px-4 py-1 text-white bg-gray-800">
+            BCV Directors
+          </button>
+        </Link>
+        <Link href="/images/certificate.pdf" passHref>
+          <button className="text-base px-4 py-1 text-white bg-gray-800">
+            BCV Certificate
+          </button>
+        </Link>
+      </div>
     </Fragment>
   );
 };
